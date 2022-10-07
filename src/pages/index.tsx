3ceae956b937b89c8afba57dcf051ff2852a071a
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-10-07 16:21:11 -0300

    login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,53 @@
-import {color, Text} from '@chakra-ui/react'
+import {Button, Center, Flex, Input, Stack} from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <Text>Dashbord</Text>
+    <Flex 
+      w="100vw" 
+      h="100vh" 
+      alignItems="center" 
+      justify="center"
+    >
+          <Flex
+            as="form"
+            width="100%"
+            maxWidth={450}
+            bg="gray.800"
+            p="8"
+            borderRadius={8}
+            flexDir="column"
+          >
+          <Stack spacing="4">
+            <Input 
+              name="email" 
+              type="email"
+              focusBorderColor='pink.500'
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+              placeholder='E-mail'
+            />
+
+            <Input 
+              name="password" 
+              type="password"
+              focusBorderColor='pink.500'
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+              placeholder='Senha'
+            />
+            </Stack>
+
+            <Button type='submit' mt="6" colorScheme="pink" size="lg">Entrar</Button>
+          
+        </Flex>
+    </Flex>
   )
 }
